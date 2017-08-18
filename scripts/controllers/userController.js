5ced57efcf5bd440d0bdc3da6fb19864dac27c11
sasan <skhayyambashi@outlook.com>
2017-08-18 11:48:04 -0400

    Adding more sorting to admin page

--- scripts/controllers/userController.js
+++ scripts/controllers/userController.js
@@ -27,4 +27,31 @@ BinaryTattooApp.controller('usersController', function usersController($scope, $
             return !$scope.searchText || (a.email && a.email.toLowerCase().includes($scope.searchText.toLowerCase()))
         }) || [];
     };
+
+    $scope.sort = function (propName) {
+        if (!$scope.sortOrder)
+            $scope.sortOrder = true;
+        else
+            $scope.sortOrder = !$scope.sortOrder;
+        $scope.m.users = $scope.m.users.sort(function (a, b) {
+            return sort(a[propName], b[propName], $scope.sortOrder);
+        });
+    }
+
+    function sort(a, b, order) {
+        if (a && b && a.toLowerCase && b.toLowerCase) {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+        }
+        if (a === b)
+            ret = 0;
+        else if (a < b) {
+            ret = order ? - 1 : 1;
+        }
+        else
+            ret = order ? 1 : -1;
+
+        //console.log(a + ' ' + b + ' ' + order + ' ' + ret);
+        return ret;
+    }
 });
